e5b5f3075b8b4458808f32ef636d97d6dbdeef27
Jack Mu <jackmu@umich.edu>
2024-12-22 22:28:23 -0800

    ok last edits tonight; building some suspense

--- agent-namer/src/app/page.tsx
+++ agent-namer/src/app/page.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col space-y-6 h-svh justify-center items-center font-['Helvetica'] text-white">
+      <div className="text-3xl font-semibold">
+        You've built your AI agent...
+      </div>
       <div className=" font-bold text-4xl ">
-        Ready to name your AI agent?
+        Are you ready to name your AI agent?
       </div>
       <AgentForm></AgentForm>
     </div>
