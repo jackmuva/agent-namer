65958df2cdc8a9b267cd7ff60d2049fd6ad8c5d7
Jack Mu <jackmu@umich.edu>
2024-12-23 12:51:31 -0800

    amplify files

---
+++ amplify-setup.sh
@@ -0,0 +1,2 @@
+aws amplify update-app --app-id d2pqi3eb51ax96 --platform WEB_COMPUTE --region us-east-1
+aws amplify update-branch --app-id d2pqi3eb51ax96 --branch-name main --framework 'Next.js - SSR' --region us-east-1
