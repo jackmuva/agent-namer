d6336829c5a475628627fb6c37f1c0aa76179373
Jack Mu <jackmu@umich.edu>
2024-12-23 12:02:30 -0800

    added postgres functionality

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,9 +1,13 @@
 import { generateNames } from ".";
+import { insertRecord } from "@/lib/postgres-utils";
 
 export async function POST(request: Request) {
 	const res = await request.json();
 	const chat = await generateNames(res.description, res.gender);
-	console.log(chat.choices[0].message);
+	if (res.website && res.website !== "") {
+		const queryStatus = await insertRecord({ website: res.website, datetime: new Date().toLocaleDateString() });
+		console.log(queryStatus);
+	}
 	return Response.json({ message: chat.choices[0].message.content })
 }
 
