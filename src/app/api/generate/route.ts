cb5b09b9120a5c65ce4a8765a20b3ff6d7e02af0
Jack Mu <jackmu@umich.edu>
2024-12-24 15:01:13 -0800

    want to keep descriptions in database as well

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -4,7 +4,7 @@ import { insertRecord } from "@/lib/postgres-utils";
 export async function POST(request: Request) {
 	const res = await request.json();
 	const chat = await generateNames(res.description, res.gender);
-	if (res.website && res.website !== "") {
+	if ((res.website && res.website !== "") || (res.description && res.description !== "")) {
 		const queryStatus = await insertRecord({ website: res.website, datetime: new Date().toLocaleDateString(), description: res.description });
 		console.log(queryStatus);
 	}
